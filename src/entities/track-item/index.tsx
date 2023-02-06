79222ef59fef7ece0c3bcc622c169301089f000d
Vladislav <typlomi@gmail.com>
2023-02-06 20:49:25 +0600

    hardcode tracks + trackitem skelet

--- src/entities/track-item/index.tsx
+++ src/entities/track-item/index.tsx
@@ -1,10 +1,31 @@
-import { Card } from "@mui/material";
+import { Card, IconButton } from "@mui/material";
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import React from "react";
 
-const TrackItem: React.FC = () => {
-  return (
-    <Card>
+interface TrackItemProps {
+  picture: string;
+  name: string;
+  artist: string;
+  album?: string;
+}
 
+const TrackItem: React.FC<TrackItemProps> = ({
+  picture, 
+  name, 
+  artist, 
+  album
+}) => {
+  return (
+    <Card className="track-item">
+      <IconButton>
+        <PlayCircleFilledWhiteIcon/>
+      </IconButton>
+      <img src={picture} alt={`${name}, ${artist}`} />
+      <div className="track-item__info">
+        <div className="track-item__name">{name}</div>
+        <div className="track-item__artist">{artist}</div>
+        <div className="track-item__album">{album}</div>
+      </div>
     </Card>
   )
 }
--- src/widgets/track-list/index.tsx
+++ src/widgets/track-list/index.tsx
@@ -1,10 +1,28 @@
+import TrackItem from "entities/track-item";
 import React from "react";
 import './styles/index.scss';
 
 const TrackList: React.FC = () => {
+  const trackList = [
+    {id: 1, name: 'Lose Yourself', artist: 'Eminem', picture: 'nkvslvndk'},
+    {id: 2, name: 'Lose Yourself', artist: 'Eminem', picture: 'nkvslvndk'},
+    {id: 3, name: 'Lose Yourself', artist: 'Eminem', picture: 'nkvslvndk'},
+    {id: 4, name: 'Lose Yourself', artist: 'Eminem', picture: 'nkvslvndk'},
+    {id: 5, name: 'Lose Yourself', artist: 'Eminem', picture: 'nkvslvndk'},
+    {id: 6, name: 'Lose Yourself', artist: 'Eminem', picture: 'nkvslvndk'},
+    {id: 7, name: 'Lose Yourself', artist: 'Eminem', picture: 'nkvslvndk'},
+  ]
+
   return (
     <div className="track-list">
-
+      {trackList.map(track => 
+        <TrackItem 
+        key={track.id}
+        name={track.name}
+        artist={track.artist} 
+        picture={track.picture}
+        />
+        )}
     </div>
   )
 }
